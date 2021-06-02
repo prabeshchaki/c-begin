2067cdc1b001a01ca0245fc2d3cf054033f13af2
Prabesh Chaki <pchaki@oneshield.com>
2021-06-02 21:39:28 +0530

    Rename File

---
+++ Increment_op.c
@@ -0,0 +1,19 @@
+// Basic increment operator concept.
+
+#include <stdio.h>
+
+int main(){
+    
+    int a = 5, b;
+    
+    b = a++;
+    printf("a ---> %d\n", a );
+    printf("b ---> %d\n", b );
+
+    b = ++a;
+    printf("a ---> %d\n", a );
+    printf("b ---> %d\n", b );
+
+    return 0;
+
+}
