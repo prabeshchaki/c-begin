eb1f1c3f0ce05f8bc22d61bf76da477dd7cb01e8
Prabesh Chaki <pchaki@oneshield.com>
2021-06-02 21:27:45 +0530

    Array & Pointers

---
+++ Arr.c
@@ -0,0 +1,15 @@
+// Play with Array and Pointers 
+
+#include <stdio.h>
+
+int main(){
+
+    int arr[5] = {1,2,3,4,5};
+
+    for ( int i = 0; i < 5 ; ++i){
+
+        printf("The value arr[%d] is %d & address is %p \n" , i , *(arr + i), (arr + i));
+
+    }
+
+}
